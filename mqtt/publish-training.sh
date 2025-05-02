d7bdd6e31d8e38fbffd28b8e11e9a9ad358768d7
Lemonday <nhathao090703@gmail.com>
2025-05-02 13:04:15 +0700

    update hyper parameters, storm memory, mqtt publisher

--- mqtt/publish-training.sh
+++ mqtt/publish-training.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 commands=(
-  "docker compose up -d building_1 building_2" # 1 2 6
+  "docker compose up -d building_1 building_2" # 1 2
   "docker compose up -d building_3 building_6" # 1 2 3 6
-  "docker compose up -d building_4 building_5" # 1 2 3 4 6
+  "docker compose up -d building_4 building_5" # 1 2 3 4 5 6
   "docker compose down building_1 building_6" # 2 3 4 6
   "docker compose down building_2 building_4" # 3 6
-  "docker compose up -d building_5 building_6" # 3 5 6
+  "docker compose up -d building_8 building_6" # 3 5 6 8
+  "docker compose up -d building_9" # 3 5 6 8 9
+  "docker compose up -d building_7" # 3 5 6 8 9 7
+  "docker compose down"
+  "docker compose up -d building_1 building_2"
+  "docker compose up -d building_3 building_4 building_6 building_8"
   "docker compose down"
   )
 
